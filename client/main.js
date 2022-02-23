ed77d68f39ce41e07b7929d8bd7073d42c5f1a87
NuhYlmz <senturknuhyilmaz@gmail.com>
2022-02-24 01:02:00 +0300

    country,language added

--- client/main.js
+++ client/main.js
@@ -5,10 +5,93 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./main.html";
 
 if (Meteor.isClient) {
+  let countiries = [
+    {
+      id: 0,
+      name: "Turkey",
+      tag: "tr",
+    },
+    {
+      id: 0,
+      name: "English",
+      tag: "en",
+    },
+    {
+      id: 0,
+      name: "Germany",
+      tag: "de",
+    },
+  ];
+  let categories = [
+    {
+      id: 0,
+      name: "General",
+      tag: "general",
+    },
+    {
+      id: 0,
+      name: "Sport",
+      tag: "sport",
+    },
+    {
+      id: 0,
+      name: "Economy",
+      tag: "economy",
+    },
+  ];
+
   Template.news.onCreated(function newsOnCreated() {
+    getNews();
+  });
+
+  Template.countiries.onCreated(function countiriesOnCreated() {
+    Session.set("selectedCountry", "tr");
+    Session.set("countiries", countiries);
+  });
+
+  Template.categories.onCreated(function categoriesOnCreated() {
+    Session.set("selectedCategory", "general");
+    Session.set("categories", categories);
+  });
+  Template.countiries.helpers({
+    countiries() {
+      return Session.get("countiries");
+    },
+  });
+  Template.categories.helpers({
+    categories() {
+      return Session.get("categories");
+    },
+  });
+  Template.countiries.events({
+    "change #country": function (event, a) {
+      Session.set("selectedCountry", event.currentTarget.value);
+      getNews();
+    },
+  });
+  Template.categories.events({
+    "change #category": function (event, a) {
+      Session.set("selectedCategory", event.currentTarget.value);
+      getNews();
+    },
+  });
+  Template.countiries.helpers({
+    countiries() {
+      return Session.get("countiries");
+    },
+  });
+  Template.news.helpers({
+    news() {
+      return Session.get("news");
+    },
+  });
+
+  function getNews() {
     HTTP.call(
       "GET",
-      "https://api.collectapi.com/news/getNews?country=tr&tag=general",
+      `https://api.collectapi.com/news/getNews?country=${Session.get(
+        "selectedCountry"
+      )}&tag=${Session.get("selectedCategory")}`,
       {
         headers: {
           "content-type": "application/json",
@@ -16,22 +99,8 @@ if (Meteor.isClient) {
         },
       },
       (error, result) => {
-        Session.set('news', JSON.parse(result.content).result);
-        
+        Session.set("news", JSON.parse(result.content).result);
       }
     );
-  });
-
-  Template.news.helpers({
-    news() {
-      return Session.get('news')
-    },
-  });
-  Template.body.events({
-    'click #country': function (event) { 
-  console.log('Template.users_insert.events click .country this._id: ' + $(event.currentTarget).find(':selected').data("id"));
-    }
-  });
-
-
+  }
 }
